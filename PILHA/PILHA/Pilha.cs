ea301b9d12ced33cedb84a5d5fad5b8dc6ae427c
Victor de Castro Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-04-29 18:16:36 -0300

    Criacao da solucao e a pasta com os codigos .cs

---
+++ PILHA/PILHA/Pilha.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PILHA
+{
+    internal class Pilha
+    {
+        
+        public Pilha() { }
+        public Pilha(Pessoa pessoa)
+        {
+
+        }
+    }
+}
---
+++ PILHA/PILHA/pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PILHA
+{
+    internal class Pessoa
+    {
+        int idade;
+        string nome;
+
+        public int getIdade
+        {
+            get { return idade; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+    }
+}
